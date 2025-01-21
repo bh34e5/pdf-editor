a413e3dbd2efc15848be967f86d9ce2d35dee4ea
bh34e5 <b.hester0614@yahoo.com>
2025-01-20 21:03:38 -0500

    open pdfs and show a page-selection bar

--- deps/shims/raygui.c
+++ deps/shims/raygui.c
@@ -1,2 +1,6 @@
 #define RAYGUI_IMPLEMENTATION
 #include "../raygui/src/raygui.h"
+
+#undef RAYGUI_IMPLEMENTATION
+#define GUI_WINDOW_FILE_DIALOG_IMPLEMENTATION
+#include "../raygui/examples/custom_file_dialog/gui_window_file_dialog.h"
